ede6f26db23fd9732a41fc56a3faad9858966d4e
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-17 21:25:29 +0600

    added data dynamically to the timeline log

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -13,31 +13,60 @@ const TimeLinePage = () => {
     return (
         <div className='w-11/12 mx-auto my-8'>
             <h2 className=" text-5xl font-bold text-[#3e490a]">Timeline</h2>
-            <div className='bg-white p-2'>sort </div>
-            <div className='space-y-4 my-4'>
-                {
-                    timelineLogs.map((timelineLog, index) => {
-                        const icon = timelineLog.label === "Text" ? "💬" :
-                            timelineLog.label === "Call" ? "📞" :
-                                timelineLog.label === "Video" ? "🎥" : '';
-    
-                        return <div className="w-full flex items-start gap-3 p-3  bg-white rounded-lg shadow-sm" key={index}>
-                           
-                            <div className="text-2xl">{icon}                       </div>
-
-                             <div className="space-y-2">
-        <p className="text-base text-gray-800">
-         <span className='font-semibold'>{timelineLog.label}</span>  with {timelineLog.specificFriend.name}
-                                </p>
-                                  <p className="text-xs text-gray-500">
-          {timelineLog.time}
-        </p>
-                         
-      </div>
-                        </div>
-                    })
-        }
+            {/* sorting dropsown  */}
+            <div className="dropdown dropdown-bottom mt-2">
+  <div tabIndex={0} role="button" className="btn m-1">Filter Timeline ⬇️</div>
+  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a>Call</a></li>
+                    <li><a>Text</a></li>
+                    <li><a >Video</a></li>
+  </ul>
             </div>
+            
+           <div className="space-y-4 my-4">
+  
+  {timelineLogs.length === 0 ? (
+    <div className="w-full h-[60vh] p-6 justify-center bg-white rounded-lg shadow-sm flex items-center">
+      <p className="text-gray-500 ">
+        No timeline logs yet 
+      </p>
+    </div>
+  ) : (
+    timelineLogs.map((timelineLog, index) => {
+      const icon =
+        timelineLog.label === "Text"
+          ? "💬"
+          : timelineLog.label === "Call"
+          ? "📞"
+          : timelineLog.label === "Video"
+          ? "🎥"
+          : "";
+
+      return (
+        <div
+          className="w-full flex items-start gap-3 p-3 bg-white rounded-lg shadow-sm"
+          key={index}
+        >
+          <div className="text-2xl">{icon}</div>
+
+          <div className="space-y-2">
+            <p className="text-base text-gray-800">
+              <span className="font-semibold">
+                {timelineLog.label}
+              </span>{" "}
+              with {timelineLog.specificFriend.name}
+            </p>
+
+            <p className="text-xs text-gray-500">
+              {timelineLog.time}
+            </p>
+          </div>
+        </div>
+      );
+    })
+  )}
+
+</div>
         </div>
     );
 };
